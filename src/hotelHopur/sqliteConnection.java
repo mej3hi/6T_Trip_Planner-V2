8cbbfe40879b4cff6ec76965c38207aa10af1018
eirikurkristinn@gmail.com <ekh1@users.noreply.github.com>
2016-04-13 15:31:44 +0000

    Allt sem við höfum frá hópum komið inn.

---
+++ src/hotelHopur/sqliteConnection.java
@@ -0,0 +1,47 @@
+package hotelHopur;
+import java.sql.*;
+import java.util.ArrayList;
+
+import javax.swing.*;
+
+
+
+public class sqliteConnection{
+	
+	static ResultSet rs;
+	static Connection conn=null;
+	static String qry;
+	public static Connection dbConnector(){
+		
+		try{
+			
+			Class.forName("org.sqlite.JDBC");
+			conn = DriverManager.getConnection("jdbc:sqlite:Hotel.db");
+			
+			return conn;
+		}catch(Exception e){
+			
+			JOptionPane.showMessageDialog(null, e);
+			return null;
+		}
+	}
+	
+	public static void closeConnection(ResultSet results, Statement statement, Connection conn){
+		try{
+			if(results!=null){
+				results.close();
+			}
+			if(statement!=null){
+				statement.close();
+			}
+			if(conn!=null){
+				conn.close();
+			}
+		}catch(Exception e){
+			System.out.println(e);
+		}
+	}
+	
+
+	
+}
--- src/trip/HotelSearch.java
+++ src/trip/HotelSearch.java
@@ -5,6 +5,7 @@
  */
 package trip;
 
+import Hotel.FinaliseBooking;
 import hotelHopur.Hotel;
 import hotelHopur.SearchControl;
 import hotelHopur.SearchMock;
@@ -24,12 +25,34 @@ public class HotelSearch {
     
     
     //???
-    public ArrayList<Hotel> search(Date datein, Date dateout, String name, String room, String address, String city, boolean wifi,
+    public ArrayList<Hotel> search(Date datein, Date dateout, String room, String city, boolean wifi,
             boolean freewifi, boolean smoke, boolean spool, boolean gym, boolean tv ){
         
-       
+        //String convertedDatein = String.format("%1$td mar. %1$tY", datein);
+        //String convertedDateout = String.format("%1$td mar. %1$tY", dateout);
+      
+        //System.out.println(convertedDatein);
+        //System.out.println(convertedDateout);
         
-        results = searchControl.LeitaHotel("r", "24 mar. 2016", "24 mar. 2016");
+        Date datein2 = new Date();
+        String dateinS = FinaliseBooking.dateToString(datein2);
+	Date dateout2 = FinaliseBooking.addDays(datein2, 10);
+	String dateoutS = FinaliseBooking.dateToString(dateout2);
+        
+        System.out.println(datein2);
+        System.out.println(dateout2);
+        
+        
+        results = searchControl.LeitaHotel(city, dateinS, dateoutS);
+        
+        if (wifi) results = searchControl.detailedSearch(results, 0);
+        if (freewifi) results = searchControl.detailedSearch(results, 1);
+        if (spool) results = searchControl.detailedSearch(results, 2);
+        if (gym) results = searchControl.detailedSearch(results, 3);
+        if (tv) results = searchControl.detailedSearch(results, 4);
+        if (smoke) results = searchControl.detailedSearch(results, 5);
+        
+        
         
         return results;
     }
